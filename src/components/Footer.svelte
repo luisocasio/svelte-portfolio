b610bf25269bf72e85751a9e0a5d64850efaf350
luis <ocasio.dev@gmail.com>
2020-08-02 00:07:46 -0400

    create footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,16 @@
+<style>
+  footer {
+    text-align: right;
+    bottom: 0;
+    width: 100%;
+    height: 50px;
+  }
+</style>
+
+<footer>
+  <p>
+    Email: Ocasio.dev@gmail.com
+    <br />
+    Phone: 717-327-0908
+  </p>
+</footer>
