f078b0e78f6401710b9aa511ad15b2720da328a0
luis <ocasio.dev@gmail.com>
2020-08-01 23:40:05 -0400

    styled nav

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -3,15 +3,23 @@
 </script>
 
 <style>
+  #title {
+    text-decoration: underline;
+  }
   #nav-bar {
     width: 100%;
     margin-top: 2.5rem;
   }
-
   #nav-items {
     display: flex;
     justify-content: space-between;
     list-style-type: none;
+  }
+  #route-links {
+    display: flex;
+    justify-content: space-evenly;
+    width: 50%;
+    font-size: 1.5rem;
   }
 </style>
 
@@ -20,11 +28,12 @@
     <ul id="nav-items">
       <li id="nav-links">
         <a href="/">
-          <h1>FULL STACK WEB DEVELOPER</h1>
+          <h1 id="title">FULL STACK WEB DEVELOPER</h1>
         </a>
+        <p>Currently seeking employment</p>
       </li>
 
-      <li>
+      <li id="route-links">
         <a href="/skills">SKILLS</a>
         <a href="/project">PROJECTS</a>
         <a href="/contact">CONTACT</a>
