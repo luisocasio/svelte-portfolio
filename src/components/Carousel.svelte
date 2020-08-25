5cfe7bc4c6bb687938331c22f71f1f8d9b0fe917
luis ocasio <ocasio@luiss-MacBook-Pro.local>
2020-08-25 07:49:19 -0400

    clean up

--- src/components/Carousel.svelte
+++ src/components/Carousel.svelte
@@ -1,68 +1,64 @@
 <script>
   import { fly, fade } from "svelte/transition";
-  let src = "./assets/svg/royalblueHeader.svg";
+  let src = "/assets/svg/royalblueHeader.svg";
   let project1 = "/assets/projects/jaems.png";
   let project2 = "/assets/projects/latte-site.jpg";
   let project3 = "/assets/projects/market.png";
   let previousSlide = "/assets/svg/back.svg";
   let nextSlide = "/assets/svg/next.svg";
-  let name = "web-sites";
-
-  const Projects = [project1, project2, project3];
-
-  const projectSite = [
-    { Jaemsound: "https://www.jaemsounds.com/" },
-    { Latteluv: "https://latteluv.netlify.app/" },
-    { MarketAve: "https://marketavenue.netlify.app/" },
-  ];
-
-  const git = [
-    { Jaemsound: "https://github.com/LuisOcasio/Jaemsounds-Gatsby-WP-AWS" },
-    { Latteluv: "https://github.com/LuisOcasio/latte-luv" },
-    { MarketAve: "https://github.com/Lambda-School-Labs/quick-street-be" },
-  ];
-
-  const about = [
-    {
-      jaemsounds:
-        "A Headless WordPress site created for a client in the music industry. This client wanted his music to be streamed and downloaded across various types of devices.   ",
-    },
-    {
-      latteluv:
-        "Single page application that gives its user a fast and snappy feel. This is possible because we are rendering our components and not having to retrieve data from a server.",
-    },
-    {
-      marketavenue:
-        "During an 8 week period I worked along 5 other developers and a UX designer to create 'Market Ave.'. An application that brings local vendors and locals closer.",
-    },
-  ];
-
-  const tech = [
-    { jaemsounds: ["Gatsby", "WordPress", "Graphql", "AWS amplify"] },
-    { latteluv: ["React", "Sass"] },
-    { marketavenue: ["React", "Express", "Node", "MongoDB"] },
-  ];
 
   let index = 0;
   let slide = 0;
 
-  $: view = Object.keys(projectSite[slide]);
-  $: url = Object.values(projectSite[slide]);
-  $: gits = Object.values(git[slide]);
-  $: abouts = Object.values(about[slide]);
-  $: techs = Object.values(tech[slide]);
+  const Projects = [project1, project2, project3];
 
   const next = () => {
     index = (index + 1) % Projects.length;
-    slide = (slide + 1) % projectSite.length;
+    slide = (slide + 1) % projects.length;
   };
 
   const previous = () => {
     if (index && slide) {
       index = (index - 1) % Projects.length;
-      slide = (slide - 1) % projectSite.length;
+      slide = (slide - 1) % projects.length;
     }
   };
+
+  const projects = [
+    { Jaemsounds: "https://www.jaemsounds.com/" },
+    { LatteLuv: "https://latteluv.netlify.app/" },
+    { MarketAvenue: "https://marketavenue.netlify.app/" },
+  ];
+
+  const projects_url = [
+    { 0: "https://github.com/LuisOcasio/Jaemsounds-Gatsby-WP-AWS" },
+    { 1: "https://github.com/LuisOcasio/latte-luv" },
+    { 2: "https://github.com/Lambda-School-Labs/quick-street-be" },
+  ];
+
+  const projects_about = [
+    {
+      0: "A Headless WordPress site created for a client in the music industry. This client wanted his music to be streamed and downloaded across various types of devices.   ",
+    },
+    {
+      1: "Single page application that gives its user a fast and snappy feel. This is possible because we are rendering our components and not having to retrieve data from a server.",
+    },
+    {
+      2: "During an 8 week period I worked along 5 other developers and a UX designer to create 'Market Avenue'. An application that brings local vendors and locals closer.",
+    },
+  ];
+
+  const technologies = [
+    { 0: ["Gatsby", "WordPress", "Graphql", "AWS amplify"] },
+    { 1: ["React", "SASS"] },
+    { 2: ["React", "mongoDB", "node", "express"] },
+  ];
+
+  $: project = Object.keys(projects[slide]);
+  $: url = Object.values(projects[slide]);
+  $: git = Object.values(projects_url[slide]);
+  $: about = Object.values(projects_about[slide]);
+  $: tech = Object.values(technologies[slide]);
 </script>
 
 <style>
@@ -99,7 +95,6 @@
     width: 80%;
     height: 85%;
   }
-
   .previous_wrapper {
     background-color: #21367f;
     height: 100%;
@@ -107,13 +102,11 @@
     justify-content: center;
     align-items: center;
   }
-
   .previous {
     width: 40px;
     z-index: 1;
     padding: 0.5rem;
   }
-
   .next_wrapper {
     background-color: #21367f;
     height: 100%;
@@ -121,13 +114,11 @@
     justify-content: center;
     align-items: center;
   }
-
   .next {
     width: 40px;
     z-index: 1;
     padding: 0.5rem;
   }
-
   .project_buttons {
     display: flex;
     justify-content: space-around;
@@ -165,6 +156,8 @@
   .text-section {
     display: flex;
     flex-direction: column;
+    width: 100%;
+    height: 100%;
   }
   .section {
     height: 100%;
@@ -198,7 +191,6 @@
     justify-content: space-around;
     font-size: small;
   }
-
   .stack_list_wrapper {
     height: 100%;
     width: 100%;
@@ -218,7 +210,7 @@
 
     <div class="slide">
       <section class="slide_title">
-        <h5>{view}</h5>
+        <h5>{project}</h5>
       </section>
 
       {#each [Projects[index]] as src (index)} <img class="carousel-image"
@@ -229,22 +221,18 @@
       <div class="text-section">
         <section class="section">
           <h5 class="section_title">About this project</h5>
-          <p class="section_paragraph">
-            {abouts}
-          </p>
+          <p class="section_paragraph">{about}</p>
         </section>
 
         <section class="section">
           <h5 class="tech_title">Tech Stack used</h5>
           <div class="stack_list_wrapper">
-            <ul class="stack_list">
-              <li>{techs}</li>
-            </ul>
+            <h5 class="stack_list">{tech}</h5>
           </div>
         </section>
 
         <div class="project_buttons">
-          <a href="{gits}">
+          <a href="{git}">
             <button class="code">Code</button>
           </a>
           <a href="{url}">
