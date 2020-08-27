0317e9586388ff63f7913efd9706816b78aec7de
luis ocasio <ocasio@luiss-MacBook-Pro.local>
2020-08-26 22:24:34 -0400

    adjust resume button

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -87,7 +87,7 @@
         text-decoration: none;
         border-radius: 35px;
         font-size: small;
-        margin-right: 2rem;
+        margin-right: 2.25rem;
         border: 2px solid #2dfafc;
     }
 </style>
